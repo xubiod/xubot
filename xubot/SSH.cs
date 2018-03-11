af9d90017b0fc40101dab9cceaf29e70dade4c03
Xubiod <pitts.davis3@hotmail.com>
2018-03-11 15:49:09 -0400

    ssh started

---
+++ xubot/SSH.cs
@@ -0,0 +1,27 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SuperSocket;
+using Renci.SshNet;
+
+namespace xubot
+{
+    public class SSH : ModuleBase
+    {
+        public static SshClient xuSSH;
+
+        [Group("ssh")]
+        public class commands : ModuleBase
+        {
+            [Command("connect")]
+            public async Task connectSSH(string host, string user, string password)
+            {
+                xuSSH = new SshClient(host, user, password);
+                await ReplyAsync("SSH client set.");
+            }
+        }
+    }
+}
