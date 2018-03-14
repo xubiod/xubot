e1e0f752f12c3288103c4dc79b446f27f9eb2ca6
Xubiod <pitts.davis3@hotmail.com>
2018-03-14 18:45:21 -0400

    my setup application is completeish

--- xubot-setup-xubiod/MainWindow.xaml.cs
+++ xubot-setup-xubiod/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,10 +26,235 @@ namespace xubot_setup_xubiod
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_Click_Men(object sender, RoutedEventArgs e)
         {
-            Start.Opacity = 0;
-            Start.IsEnabled = false;
+            Start.Visibility = Visibility.Collapsed;
+            Review.Visibility = Visibility.Collapsed;
+
+            Options.Visibility = Visibility.Visible;
+        }
+
+        private void Button_Click_Opt(object sender, RoutedEventArgs e)
+        {
+            Options.Visibility = Visibility.Collapsed;
+
+            Discord.Visibility = Visibility.Visible;
+        }
+
+        private void Button_Click_Discord(object sender, RoutedEventArgs e)
+        {
+            Discord.Visibility = Visibility.Collapsed;
+
+            if ((bool)reddit.IsChecked)
+            {
+                Reddit_Grid.Visibility = Visibility.Visible;
+            }
+            else if ((bool)twitter.IsChecked)
+            {
+                Twitter_Grid.Visibility = Visibility.Visible;
+            }
+            else if ((bool)googleShorten.IsChecked)
+            {
+                Google_Grid.Visibility = Visibility.Visible;
+            }
+            else if ((bool)cat.IsChecked)
+            {
+                Cat_Grid.Visibility = Visibility.Visible;
+            }
+            else if ((bool)cat.IsChecked)
+            {
+                Cat_Grid.Visibility = Visibility.Visible;
+            }
+            else if ((bool)steam.IsChecked)
+            {
+                Steam_Grid.Visibility = Visibility.Visible;
+            }
+            else if ((bool)github.IsChecked)
+            {
+                GitHub_Grid.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                Review.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Button_Click_Reddit(object sender, RoutedEventArgs e)
+        {
+            Reddit_Grid.Visibility = Visibility.Collapsed;
+
+            if ((bool)twitter.IsChecked)
+            {
+                Twitter_Grid.Visibility = Visibility.Visible;
+            }
+            else if ((bool)googleShorten.IsChecked)
+            {
+                Google_Grid.Visibility = Visibility.Visible;
+            }
+            else if ((bool)cat.IsChecked)
+            {
+                Cat_Grid.Visibility = Visibility.Visible;
+            }
+            else if ((bool)cat.IsChecked)
+            {
+                Cat_Grid.Visibility = Visibility.Visible;
+            }
+            else if ((bool)steam.IsChecked)
+            {
+                Steam_Grid.Visibility = Visibility.Visible;
+            }
+            else if ((bool)github.IsChecked)
+            {
+                GitHub_Grid.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                Review.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Button_Click_Twitter(object sender, RoutedEventArgs e)
+        {
+            Twitter_Grid.Visibility = Visibility.Collapsed;
+
+            if ((bool)googleShorten.IsChecked)
+            {
+                Google_Grid.Visibility = Visibility.Visible;
+            }
+            else if ((bool)cat.IsChecked)
+            {
+                Cat_Grid.Visibility = Visibility.Visible;
+            }
+            else if ((bool)cat.IsChecked)
+            {
+                Cat_Grid.Visibility = Visibility.Visible;
+            }
+            else if ((bool)steam.IsChecked)
+            {
+                Steam_Grid.Visibility = Visibility.Visible;
+            }
+            else if ((bool)github.IsChecked)
+            {
+                GitHub_Grid.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                Review.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Button_Click_Google(object sender, RoutedEventArgs e)
+        {
+            Google_Grid.Visibility = Visibility.Collapsed;
+
+            if ((bool)cat.IsChecked)
+            {
+                Cat_Grid.Visibility = Visibility.Visible;
+            }
+            else if ((bool)steam.IsChecked)
+            {
+                Steam_Grid.Visibility = Visibility.Visible;
+            }
+            else if ((bool)github.IsChecked)
+            {
+                GitHub_Grid.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                Review.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Button_Click_Cat(object sender, RoutedEventArgs e)
+        {
+            Cat_Grid.Visibility = Visibility.Collapsed;
+
+            if ((bool)steam.IsChecked)
+            {
+                Steam_Grid.Visibility = Visibility.Visible;
+            }
+            else if ((bool)github.IsChecked)
+            {
+                GitHub_Grid.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                Review.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Button_Click_Steam(object sender, RoutedEventArgs e)
+        {
+            Steam_Grid.Visibility = Visibility.Collapsed;
+
+            if ((bool)github.IsChecked)
+            {
+                GitHub_Grid.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                Review.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Button_Click_Github(object sender, RoutedEventArgs e)
+        {
+            GitHub_Grid.Visibility = Visibility.Collapsed;
+
+            Review.Visibility = Visibility.Visible;
+            
+        }
+
+        private void SaveFile(object sender, RoutedEventArgs e)
+        {
+            string json = @"{
+    ""discord"": """ + discordToken.Text + @""",
+    ""discord_dev"": """ + discordToken.Text + @""",
+
+    ""reddit"": {
+        ""user"": """ + reddit_username.Text + @""",
+        ""pass"": """ + reddit_pass.Text + @""",
+        ""key1"": """ + reddit_id.Text + @""",
+        ""key2"": """ + reddit_secret.Text + @""",
+    },
+
+    ""twitter"": {
+        ""key1"": """ + twitter_consumerkey.Text + @""",
+        ""key2"": """ + twitter_consumersec.Text + @""",
+        ""key3"": """ + twitter_uat.Text + @""",
+        ""key4"": """ + twitter_uak.Text + @""",
+    },
+
+    ""googleLinkShort"": """ + googlKey.Text + @""",
+    ""cat"": """ + catKey.Text + @""",
+    ""steam"": """ + steamKey.Text + @""",
+    ""github:"": """ + ghpat.Text + @"""
+}";
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.FileName = "Keys"; // Default file name
+            dlg.DefaultExt = ".json"; // Default file extension
+            dlg.Filter = "JSON files (.json)|*.json"; // Filter files by extension
+            //dlg.CheckFileExists = true;
+
+            // Show save file dialog box
+            bool? result = dlg.ShowDialog();
+
+            // Process save file dialog box results
+            if (result == true)
+            {
+                // Save document
+                string filename = dlg.FileName;
+
+                if (File.Exists(filename))
+                {
+                    File.Delete(filename);
+                }
+
+                File.WriteAllText(filename, json);
+
+                Review.Visibility = Visibility.Collapsed;
+                Complete.Visibility = Visibility.Visible;
+            }
         }
     }
 }
