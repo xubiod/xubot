771b42806e5c3edfbdc1b5f20e0b17375f449d92
Xubiod <pitts.davis3@hotmail.com>
2018-05-01 17:34:28 -0400

    updated imagesharp and started canvas

---
+++ xubot/src/Canvas.cs
@@ -0,0 +1,26 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using SixLabors.ImageSharp;
+using SixLabors.Primitives;
+using SixLabors.Shapes;
+using SixLabors.ImageSharp.Processing;
+
+using SLImage = SixLabors.ImageSharp.Image;
+
+namespace xubot.src
+{
+    [Group("canvas")]
+    public class Canvas : ModuleBase
+    {
+        [Command("create")]
+        public async Task create(int width, int height)
+        {
+            
+        }
+    }
+}
