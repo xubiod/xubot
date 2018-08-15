0b8b5fe1f1d385655377524d2edba5e6411a5ede
Xubiod <pitts.davis3@hotmail.com>
2018-08-15 17:43:34 -0400

    NEW UPDATER WOOO

---
+++ xubot-launcher/Program.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using FluentFTP;
+using Newtonsoft.Json.Linq;
+using System.IO.Compression;
+using Ionic.Zip;
+using System.Diagnostics;
+
+namespace xubot_launcher
+{
+    class Program
+    {
+        public static dynamic keys;
+
+        static void Main(string[] args)
+        {
+            string currentDir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            keys = JObject.Parse(File.ReadAllText(Path.Combine(currentDir, "data.json")));
+
+            Console.WriteLine("* xubot launcher + updater * * *");
+            Console.WriteLine();
+            Console.WriteLine("logging into local update server via FTP on port 22...");
+            
+            FtpClient client = new FtpClient(keys.uri.ToString(), keys.user.ToString(), keys.pass.ToString());
+            client.Connect();
+
+            Console.WriteLine("connection successful: logged in as " + client.Credentials.UserName);
+            Console.WriteLine();
+
+            if (client.FileExists(keys.location.ToString() + "/xubot_win7.zip"))
+            {
+                Console.WriteLine("file exists, downloading update");
+                client.DownloadFile(currentDir + "/xubot_win7.zip", keys.location.ToString() + "/xubot_win7.zip",true);
+
+                Console.WriteLine("download complete, renaming remote file");
+                client.MoveFile(keys.location.ToString() + "/xubot_win7.zip", keys.location.ToString() + "/xubot_win7_OLD.zip");
+
+                Console.WriteLine("disconnecting");
+                client.Disconnect();
+
+
+                // unpack file
+
+                string zipPath = currentDir + "/xubot_win7.zip";
+                string extractPath = currentDir + "/xubot";
+
+                Console.WriteLine("unpacking file");
+
+                using (ZipFile zip = ZipFile.Read(zipPath))
+                {
+                    zip.ExtractAll(extractPath);
+                }
+
+                Console.WriteLine("file extracted, launching xubot");
+
+            } else
+            {
+                Console.WriteLine(keys.location.ToString() + "/xubot_win7.zip not found, probably latest build, disconnecting");
+                client.Disconnect();
+            }
+
+            Process.Start(currentDir + "\\xubot\\Debug\\xubot.exe");
+        }
+    }
+}
---
+++ xubot/src/Update.cs
@@ -0,0 +1,30 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xubot.src
+{
+    public class Update : ModuleBase
+    {
+        [Command("update")]
+        public async Task update()
+        {
+            string launcher_path = Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).FullName).FullName + "\\xubot-launcher.exe";
+
+            if (File.Exists(launcher_path))
+            {
+                Process.Start(launcher_path);
+                Environment.Exit(0);
+            } else
+            {
+                await ReplyAsync("The updater executable isn't where it should be. :(");
+                await ReplyAsync(launcher_path);
+            }
+        }
+    }
+}
