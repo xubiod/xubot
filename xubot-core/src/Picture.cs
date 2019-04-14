67c276ea749de8d93433dce2fa7cef467aedb067
Xubiod <pitts.davis3@hotmail.com>
2019-04-14 15:34:21 -0400

    removed nonexistant packages

--- xubot-core/src/Picture.cs
+++ xubot-core/src/Picture.cs
@@ -4,7 +4,6 @@ using Discord.Commands;
 using System.Net.Http;
 using System.Drawing;
 using System.IO;
-using IronOcr;
 using System.Threading;
 using Discord;
 using System.IO.Compression;
@@ -24,39 +23,6 @@ namespace xubot_core.src
         [Group("pic")]
         public class music_comm : ModuleBase
         {
-            /*
-            [Command(RunMode = RunMode.Async)]
-            public async Task read(string img)
-            {
-                var msg = await ReplyAsync("Please wait, this takes a while to read images.");
-
-                Random rnd = new Random();
-                int rnd_res = rnd.Next(99999);
-                string[] imgSplit = img.Split('.');
-                string fileType = imgSplit[imgSplit.Length - 1];
-                fileType = "." + fileType;
-                string path = Path.Combine(Path.GetTempPath(), rnd_res.ToString()) + fileType;
-
-                System.Drawing.Image imgFromStream = null;
-
-                using (HttpClient client = new HttpClient())
-                using (HttpResponseMessage response = await client.GetAsync(img))
-                using (HttpContent content = response.Content)
-                {
-                    imgFromStream = System.Drawing.Image.FromStream(await content.ReadAsStreamAsync());
-                    Bitmap bitmap = (Bitmap)imgFromStream;
-
-                    bitmap.Save(path);
-
-                    var Ocr = new AutoOcr();
-                    var Result = Ocr.Read(path);
-                    Console.WriteLine(Result.Text);
-
-                    await msg.DeleteAsync();
-                    await ReplyAsync("**(Automatic) Iron OCR returned:\n** " + Result);
-                }
-            }*/
-
             [Group("manip")]
             public class manipulation : ModuleBase
             {
--- xubot-core/src/SSH.cs
+++ xubot-core/src/SSH.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using SuperSocket;
 using Renci.SshNet;
 using System.IO;
 using System.Threading;
