4f27c820c90f6a1eb38901ebd23433bdc725fbda
Xubiod <pitts.davis3@hotmail.com>
2019-05-18 09:54:58 -0400

    replace my shit code with someone else's basically :')

---
+++ xubot-core/src/SteamKitInteg.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using SteamKit2;
+
+namespace xubot_core.src
+{
+    [Group("steam"), Summary("Steam API integration via SteamKit2.")]
+    public class SteamKitInteg : ModuleBase
+    {
+        [Command("user"), Summary("Gets information about a Steam user based on their ID.")]
+        public async Task User(string id)
+        {
+            dynamic playerService = WebAPI.GetInterface("IPlayerService", Program.keys.steam.ToString());
+
+            KeyValue ownedGames = playerService.GetOwnedGames( steamid: id );
+
+            await ReplyAsync(ownedGames["game_count"].AsString());
+        }
+    }
+}
