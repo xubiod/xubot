7b29ceefdb0a67cb9dbb830aa2bdca893704f223
Xubiod <pitts.davis3@hotmail.com>
2019-05-18 17:45:09 -0400

    it's 500x times better for fucks sake

--- xubot-core/src/SteamKitInteg.cs
+++ xubot-core/src/SteamKitInteg.cs
@@ -11,23 +11,30 @@ namespace xubot_core.src
     [Group("steam"), Summary("Steam API integration via SteamKit2.")]
     public class SteamKitInteg : ModuleBase
     {
-        [Command("user"), Summary("Gets information about a Steam user based on their ID.")]
-        public async Task User(string id)
+        static dynamic steamUserInterface = WebAPI.GetInterface("ISteamUser", Program.keys.steam.ToString());
+        static dynamic playerServiceInterface = WebAPI.GetInterface("IPlayerService", Program.keys.steam.ToString());
+        static dynamic steamAppsInterface = WebAPI.GetInterface("ISteamApps", Program.keys.steam.ToString());
+
+        [Command("user", RunMode = RunMode.Async), Summary("Gets information about a Steam user based on their ID.")]
+        public async Task User(ulong id)
         {
             try {
-                dynamic playerService = WebAPI.GetInterface("IPlayerService", Program.keys.steam.ToString());
-                dynamic steamUser = WebAPI.GetInterface("ISteamUser", Program.keys.steam.ToString());
-
-                KeyValue ownedGames = playerService.GetOwnedGames(steamid: id);
-                KeyValue playerSummaries = steamUser.GetPlayerSummaries002(steamids: id);
+                KeyValue ownedGames = playerServiceInterface.GetOwnedGames(steamid: id);
+                KeyValue playerSummaries = steamUserInterface.GetPlayerSummaries002(steamids: id);
 
                 playerSummaries = playerSummaries["players"].Children[0];
 
-                int twoWeeks = 0;
-                int forever = 0;
+                decimal twoWeeks = 0;
+                decimal forever = 0;
 
-                string mostTimeIn = "";
-                int mostTime = 0;
+                int mostTimeIn = 0;
+                decimal mostTime = 0;
+
+                int mostWeekIn = 0;
+                decimal mostWeek = 0;
+
+                EmbedFieldBuilder mostWeekField = new EmbedFieldBuilder { Name = "Most Playtime (last 2 weeks)", Value = "Has not played in last 2 weeks.", IsInline = true };
+                EmbedFieldBuilder mostTimeField = new EmbedFieldBuilder { Name = "Most Playtime (forever)", Value = "Has not played since account creation (Wha...?)", IsInline = true };
 
                 foreach (KeyValue game in ownedGames["games"].Children)
                 {
@@ -37,8 +44,24 @@ namespace xubot_core.src
                     if (game["playtime_forever"].AsInteger(0) > mostTime)
                     {
                         mostTime = game["playtime_forever"].AsInteger(0);
-                        mostTimeIn = game["appid"].AsString();
+                        mostTimeIn = game["appid"].AsInteger();
                     }
+
+                    if (game["playtime_2weeks"].AsInteger(0) > mostWeek)
+                    {
+                        mostWeek = game["playtime_forever"].AsInteger(0);
+                        mostWeekIn = game["appid"].AsInteger();
+                    }
+                }
+
+                if (mostWeekIn != 0)
+                {
+                    mostWeekField.Value = "In App **" + ReturnAppName(mostWeekIn) + "**: " + string.Format("{0:#,###0.0}", mostWeek) + " minutes\n" + string.Format("{0:#,###0.0}", mostWeek / 60) + " hours";
+                }
+
+                if (mostTimeIn != 0)
+                {
+                    mostTimeField.Value = "In App **" + ReturnAppName(mostTimeIn) + "**: " + string.Format("{0:#,###0.0}", mostTime) + " minutes\n" + string.Format("{0:#,###0.0}", mostTime / 60) + " hours";
                 }
 
                 ulong _lastLogOff = playerSummaries["lastlogoff"].AsUnsignedLong(0);
@@ -73,21 +96,19 @@ namespace xubot_core.src
                                 new EmbedFieldBuilder
                                 {
                                     Name = "Playtime (last 2 weeks)",
-                                    Value = string.Format("{0:#,###0}", twoWeeks) + " minutes\n" + string.Format("{0:#,###0}", twoWeeks/60) + " hours",
+                                    Value = string.Format("{0:#,###0.0}", twoWeeks) + " minutes\n" + string.Format("{0:#,###0.0}", twoWeeks/60) + " hours",
                                     IsInline = true
                                 },
                                 new EmbedFieldBuilder
                                 {
                                     Name = "Playtime (forever)",
-                                    Value = string.Format("{0:#,###0}", forever) + " minutes\n" + string.Format("{0:#,###0}", forever/60) + " hours\n" + string.Format("{0:#,###0}", forever/1440) + " days",
+                                    Value = string.Format("{0:#,###0.0}", forever) + " minutes\n" + string.Format("{0:#,###0.0}", forever/60) + " hours\n" + string.Format("{0:#,###0.0}", forever/1440) + " days",
                                     IsInline = true
                                 },
-                                new EmbedFieldBuilder
-                                {
-                                    Name = "Most Playtime",
-                                    Value = "In App " + mostTimeIn + ": " + string.Format("{0:#,###0}", mostTime) + " minutes\n" + string.Format("{0:#,###0}", mostTime/60) + " hours",
-                                    IsInline = true
-                                },
+                                mostWeekField
+                                ,
+                                mostTimeField
+                                ,
                                 new EmbedFieldBuilder
                                 {
                                     Name = "Last Logoff",
@@ -111,6 +132,32 @@ namespace xubot_core.src
             {
                 await GeneralTools.CommHandler.BuildError(ex, Context);
             }
+        }
+
+        [Command("user", RunMode = RunMode.Async), Summary("Gets information about a Steam user based on their vanity URL.")]
+        public async Task User(string vanity)
+        {
+            KeyValue vanityUrl = steamUserInterface.ResolveVanityURL(vanityurl: vanity);
+
+            await User(vanityUrl["steamid"].AsUnsignedLong(0));
+        }
+
+        public static string ReturnAppName(int appID)
+        {
+            KeyValue appList = steamAppsInterface.GetAppList2()["apps"];
+
+            KeyValue app = appList.Children.Find(x => x["appid"].AsInteger() == appID);
+
+            return app["name"].AsString();
+        }
+
+        public static int ReturnAppID(string appName)
+        {
+            KeyValue appList = steamAppsInterface.GetAppList2()["apps"];
+
+            KeyValue app = appList.Children.Find(x => x["name"].AsString() == appName);
+
+            return app["appid"].AsInteger();
         }
     }
 }
