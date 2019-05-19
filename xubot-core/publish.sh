099522ec5c41074b50890cf3d7fe68d6c56ab22e
Xubiod <pitts.davis3@hotmail.com>
2019-05-18 22:13:06 -0400

    added comments to explain it to myself

--- xubot-core/publish.sh
+++ xubot-core/publish.sh
@@ -1,3 +1,12 @@
+# publish xubot to /home/[USER]/xubot-home
+# this is to provide a consistent home for xubot
 dotnet publish --configuration Release --output ~/xubot-home
+
+# move to xubot's home
+# this instance it's not breaking and entering
+# don't worry, as it's also your home
 cd ~/xubot-home
+
+# run xubot in the background
+# this also allows the ability to close the SSH shell
 nohup dotnet xubot-core.dll
