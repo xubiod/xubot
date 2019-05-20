dcef664ac573a9616ca5af6c3c8fb55fdbde6888
Xubiod <pitts.davis3@hotmail.com>
2019-05-20 17:38:00 -0400

    update skeleton made

--- xubot-core/src/Update.cs
+++ xubot-core/src/Update.cs
@@ -7,27 +7,16 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LibGit2Sharp;
 
 namespace xubot_core.src
 {
     public class Update : ModuleBase
     {
         [Command("update"), RequireOwner]
-        public async Task update()
+        public async Task UpdateBot()
         {
-            string launcher_path = Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).FullName).FullName + "\\xubot-launcher.exe";
-
-            if (File.Exists(launcher_path))
-            {
-                await Program.xuClient.SetStatusAsync(UserStatus.Invisible);
-                Process.Start(launcher_path);
-                Environment.Exit(0);
-            }
-            else
-            {
-                await ReplyAsync("The updater executable isn't where it should be. :(");
-                await ReplyAsync(launcher_path);
-            }
+            // to be redone
         }
     }
 }
