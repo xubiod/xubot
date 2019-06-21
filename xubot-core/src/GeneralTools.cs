b2f00e3626ab63210c7fa3f22ca2517b56fda993
Xubiod <pitts.davis3@hotmail.com>
2019-06-21 14:41:19 -0400

    added to SyntaxHighlightify

--- xubot-core/src/GeneralTools.cs
+++ xubot-core/src/GeneralTools.cs
@@ -352,10 +352,14 @@ namespace xubot_core.src
             switch (input)
             {
                 case "System.String": return "string";
-                case "System.Int32": return "int";
                 case "System.Boolean": return "bool";
+                case "System.Decimal": return "decimal";
                 case "System.Single": return "float";
                 case "System.Double": return "double";
+                case "System.Int32": return "int";
+                case "System.UInt32": return "uint";
+                case "System.Int64": return "long";
+                case "System.UInt64": return "ulong";
 
                 default: return input;
             }
