d907a4baa49eb571b79916e353faa580ae67f3d5
Xubiod <pitts.davis3@hotmail.com>
2019-07-31 18:42:18 -0400

    more stuff whoaw

--- xubot-core/src/Shitpost.cs
+++ xubot-core/src/Shitpost.cs
@@ -29,10 +29,14 @@ namespace xubot_core.src
         public static int Size, X, Y;
         public static string Text;
 
-        [Command("text-overlay", RunMode = RunMode.Async), Summary("Overlays text on an image. The parameter string has a very specific format that **must** be followed: ```\"x,y,text,size\"```")]
-        public async Task ShitpostCmd(string parameter)
+        public static int Wraparound;
+        public static int R, G, B;
+
+        [Command("text-overlay", RunMode = RunMode.Async), Summary("Overlays text on an image. The parameter string has a very specific format that **must** be followed: ```\"x,y,text,size\"```The optional parameter has a specific format too: ```\"textwrap width, r, g, b\"```")]
+        public async Task ShitpostCmd(string parameter, string optional = "")
         {
             InterpParameters(parameter);
+            if (optional != "") InterpOptionalParameters(optional);
 
             await GeneralTools.DownloadAttachmentAsync(Context, Path.GetTempPath() + "textoverlay", true);
             string type = Path.GetExtension(GeneralTools.ReturnAttachmentURL(Context));
@@ -43,7 +47,14 @@ namespace xubot_core.src
             using (Image<Rgba32> container = new Image<Rgba32>(img.Width * 5, img.Height * 5))
             {
                 container.Mutate(mut => mut.DrawImage(img, new Point(img.Width * 2, img.Height * 2), PixelColorBlendingMode.Normal, 1.0F));
-                container.Mutate(mut => mut.DrawText(Text, font, Rgba32.Black, new PointF((img.Width * 2) + X, (img.Height * 2) + Y)));
+                if (optional == "")
+                {
+                    container.Mutate(mut => mut.DrawText(Text, font, Rgba32.Black, new PointF((img.Width * 2) + X, (img.Height * 2) + Y)));
+                }
+                else
+                {
+                    container.Mutate(mut => mut.DrawText(new TextGraphicsOptions() { WrapTextWidth = Wraparound, ColorBlendingMode = PixelColorBlendingMode.Normal },Text, font, new Rgba32(R/255, G/255, B/255), new PointF((img.Width * 2) + X, (img.Height * 2) + Y)));
+                }
 
                 // proper cropping
                 container.Mutate(mut => mut.Crop(new Rectangle(img.Width * 2, img.Height * 2, img.Width, img.Height)));
@@ -65,6 +76,16 @@ namespace xubot_core.src
             Y = int.Parse(split[1]);
             Text = split[2];
             Size = int.Parse(split[3]);
+        }
+
+        public static void InterpOptionalParameters(string input)
+        {
+            string[] split = input.Split(",");
+
+            Wraparound = int.Parse(split[0]);
+            R = int.Parse(split[1]);
+            G = int.Parse(split[2]);
+            B = int.Parse(split[3]);
         }
     }
 }
