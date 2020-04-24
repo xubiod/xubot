69bfa689f7a79345168c1d4b45d9c08931e9d00e
xubiod <xubiod.mail@gmail.com>
2020-04-24 09:01:06 -0400

    attribute holy shit

---
+++ xubot-core/src/Attributes/DeprecatedAttribute.cs
@@ -0,0 +1,13 @@
+﻿using SteamKit2.Unified.Internal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xubot_core.src.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class DeprecatedAttribute : Attribute
+    {
+        public DeprecatedAttribute() {}
+    }
+}
