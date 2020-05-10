a2a4e4c5133dbf023bc315a69b357d3775919678
xubiod <xubiod.mail@gmail.com>
2020-05-10 19:49:30 -0400

    seems like a good idea

---
+++ xubot-core/src/Attributes/NSFWPossibiltyAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tweetinvi.Logic.DTO;
+
+namespace xubot_core.src.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+    public class NSFWPossibiltyAttribute : Attribute
+    {
+        public string warnings { get; }
+
+        public NSFWPossibiltyAttribute()
+        {
+            this.warnings = "Unspecified";
+        }
+
+        public NSFWPossibiltyAttribute(string warnings)
+        {
+            this.warnings = warnings;
+        }
+
+        public string GetWarnings()
+        {
+            return warnings;
+        }
+    }
+}
