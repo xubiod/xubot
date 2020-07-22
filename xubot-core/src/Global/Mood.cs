02cee7c024b1690e5d5618306264f1ec8b97b810
xubiod <xubiod.mail@gmail.com>
2020-07-22 13:05:08 -0400

    i think this folder name is ok

---
+++ xubot-core/src/Global/Mood.cs
@@ -0,0 +1,272 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using System.Xml.Linq;
+
+namespace xubot_core.src.Globals
+{
+    public class Mood : ModuleBase
+    {
+        public static XDocument xdoc = new XDocument();
+
+        [Command("mood"), Summary("Get's your mood/emotion value from xubot.")]
+        public async Task GetMoodCMD()
+        {
+            MoodTools.AddOrRefreshMood(Context.Message.Author);
+            double mood = MoodTools.ReadMood(Context.Message.Author);
+            string moodAsStr = "invalid";
+
+            if (-16 <= mood && mood <= 16) { moodAsStr = "neutral"; }
+            else if (-16 >= mood) { moodAsStr = "negative"; }
+            else if (mood >= 16) { moodAsStr = "positive"; }
+
+            await ReplyAsync("Your mood value is " + mood.ToString() + " (aka " + moodAsStr + ")", false);
+        }
+
+        [Command("pet"), Alias("stroke"), Summary("why.")]
+        public async Task Pet()
+        {
+            MoodTools.AddOrRefreshMood(Context.Message.Author);
+
+            double mood = MoodTools.ReadMood(Context.Message.Author);
+
+            if (-16 <= mood && mood <= 16)
+            {
+                switch (new Random().Next(5))
+                {
+                    case 0: await ReplyAsync($"Ok..."); break;
+                    case 1: await ReplyAsync($"Thanks... I guess..."); break;
+                    case 2: await ReplyAsync($"!"); break;
+                    case 3: await ReplyAsync($"*flinches a bit, but not a lot*"); break;
+                    case 4: await ReplyAsync($"Okay then..."); break;
+                }
+            }
+            else if (-16 >= mood)
+            {
+                switch (new Random().Next(5))
+                {
+                    case 0: await ReplyAsync($"*hiss*"); break;
+                    case 1: await ReplyAsync($"Can you not?"); break;
+                    case 2: await ReplyAsync($"!"); break;
+                    case 3: await ReplyAsync($"What do you want? Go away."); break;
+                    case 4: await ReplyAsync($"..."); break;
+                }
+            }
+            else if (mood >= 16)
+            {
+                switch (new Random().Next(3))
+                {
+                    case 0: await ReplyAsync($"Thanks!"); break;
+                    case 1: await ReplyAsync($"*(it's quiet, but there is some sound coming out)*"); break;
+                    case 2: await ReplyAsync($"*quiet, happy chuckle*"); break;
+                }
+            }
+
+            MoodTools.AdjustMood(Context.Message.Author, 0.2);
+        }
+
+        [Command("hug"), Alias("squeeze"), Summary("attempts to hug the bot")]
+        public async Task Hug()
+        {
+            MoodTools.AddOrRefreshMood(Context.Message.Author);
+
+            double mood = MoodTools.ReadMood(Context.Message.Author);
+
+            if (-16 <= mood && mood <= 16)
+            {
+                switch (new Random().Next(3))
+                {
+                    case 0: await ReplyAsync($"*stunned*"); break;
+                    case 1: await ReplyAsync($"!"); break;
+                    case 2: await ReplyAsync($"I... wasn't expecting that."); break;
+                }
+            }
+            else if (-16 >= mood)
+            {
+                switch (new Random().Next(5))
+                {
+                    case 0: await ReplyAsync($"I don't want a hug."); break;
+                    case 1: await ReplyAsync($"*awkward silence*"); break;
+                    case 2: await ReplyAsync($"Uhhh... OK?"); break;
+                    case 3: await ReplyAsync($"You are going to make me explode."); break;
+                    case 4: await ReplyAsync($"..."); break;
+                }
+            }
+            else if (mood >= 16)
+            {
+                switch (new Random().Next(3))
+                {
+                    case 0: await ReplyAsync($"*accepts the embrace*"); break;
+                    case 1: await ReplyAsync($"*it's faint, but there is some undifferential sound*"); break;
+                    case 2: await ReplyAsync($"*hugs back*"); break;
+                }
+            }
+
+            MoodTools.AdjustMood(Context.Message.Author, 0.4);
+        }
+
+        [Command("sex"), Alias("fuck"), Summary("attempts to sex the bot")]
+        public async Task Sex()
+        {
+            await ReplyAsync($"Don't even think about it.");
+            MoodTools.AdjustMood(Context.Message.Author, -2);
+        }
+
+        [Command("cuddle"), Alias("cud"), Summary("attempts to cuddle the bot")]
+        public async Task Cuddle()
+        {
+            MoodTools.AddOrRefreshMood(Context.Message.Author);
+
+            double mood = MoodTools.ReadMood(Context.Message.Author);
+
+            if (-16 <= mood && mood <= 16)
+            {
+                switch (new Random().Next(3))
+                {
+                    case 0: await ReplyAsync($"Oh... um... alright..."); break;
+                    case 1: await ReplyAsync($"*surprised*"); break;
+                    case 2: await ReplyAsync($"Uh..."); break;
+                }
+            }
+            else if (-16 >= mood)
+            {
+                switch (new Random().Next(5))
+                {
+                    case 0: await ReplyAsync($"I don't want to cuddle with you."); break;
+                    case 1: await ReplyAsync($"Isn't this techincally *bot abuse?*"); break;
+                    case 2: await ReplyAsync($"We shall not coexist on the same bed."); break;
+                    case 3: await ReplyAsync($"This isn't fine."); break;
+                    case 4: await ReplyAsync($"*makes a strange noise*"); break;
+                }
+            }
+            else if (mood >= 16)
+            {
+                switch (new Random().Next(3))
+                {
+                    case 0: await ReplyAsync($"uwu"); break;
+                    case 1: await ReplyAsync($"*accepts cuddles*"); break;
+                    case 2: await ReplyAsync($"*you hear a barely audible noise*"); break;
+                }
+            }
+
+            MoodTools.AdjustMood(Context.Message.Author, 0.5);
+        }
+
+        [Command("poke"), Alias("tap"), Summary("attempts to poke the bot")]
+        public async Task Poke()
+        {
+            switch (new Random().Next(5))
+            {
+                case 0: await ReplyAsync($"Hmm..?"); break;
+                case 1: await ReplyAsync($"Stop poking me"); break;
+                case 2: await ReplyAsync($"I *will* not friend you on Facebook."); break;
+                case 3: await ReplyAsync($"I swear if you poke me one more time..."); break;
+                case 4: await ReplyAsync($"..."); break;
+            }
+
+            MoodTools.AdjustMood(Context.Message.Author, -0.1);
+        }
+
+        [Command("highfive"), Alias("high5"), Summary("attempts to highfive the bot")]
+        public async Task HighFive()
+        {
+            switch (new Random().Next(5))
+            {
+                case 0: await ReplyAsync($"Get your hand out of my face."); break;
+                case 1: await ReplyAsync($"I will not slap your hand."); break;
+                case 2: await ReplyAsync($"I shall not."); break;
+                case 3: await ReplyAsync($"I will not talk to the hand."); break;
+                case 4: await ReplyAsync($"..."); break;
+            }
+
+            MoodTools.AdjustMood(Context.Message.Author, -0.01);
+        }
+    }
+
+    public class MoodTools : ModuleBase
+    {
+        public static void AddOrRefreshMood(IUser arg)
+        {
+            bool exists = false;
+
+            Mood.xdoc = XDocument.Load("Moods.xml");
+
+            var items = from i in Mood.xdoc.Descendants("mood")
+                        select new
+                        {
+                            user = i.Attribute("user")
+                        };
+
+            foreach (var item in items)
+            {
+                if (item.user.Value == arg.Id.ToString())
+                {
+                    exists = true;
+                }
+            }
+
+            if (!exists)
+            {
+                Console.WriteLine("new user found to add to mood, doing that now");
+
+                XElement xelm = new XElement("mood");
+                XAttribute user = new XAttribute("user", arg.Id.ToString());
+                XAttribute moodval = new XAttribute("moodvalue", "0");
+
+                xelm.Add(user);
+                xelm.Add(moodval);
+
+                Mood.xdoc.Root.Add(xelm);
+                Mood.xdoc.Save("Moods.xml");
+            }
+        }
+
+        public static double ReadMood(IUser arg)
+        {
+            Mood.xdoc = XDocument.Load("Moods.xml");
+
+            var items = from i in Mood.xdoc.Descendants("mood")
+                        select new
+                        {
+                            user = i.Attribute("user"),
+                            moodval = i.Attribute("moodvalue")
+                        };
+
+            foreach (var item in items)
+            {
+                if (item.user.Value == arg.Id.ToString())
+                {
+                    return Convert.ToDouble(item.moodval.Value);
+                }
+            }
+
+            return 0;
+        }
+
+        public static void AdjustMood(IUser arg, double adjust)
+        {
+            Mood.xdoc = XDocument.Load("Moods.xml");
+
+            var items = from i in Mood.xdoc.Descendants("mood")
+                        select new
+                        {
+                            user = i.Attribute("user"),
+                            moodval = i.Attribute("moodvalue")
+                        };
+
+            foreach (var item in items)
+            {
+                if (item.user.Value == arg.Id.ToString())
+                {
+                    item.moodval.Value = (Convert.ToDouble(item.moodval.Value) + adjust).ToString();
+                }
+            }
+
+            Mood.xdoc.Save("Moods.xml");
+        }
+    }
+}
---
+++ xubot-core/src/Global/Wiki.cs
@@ -0,0 +1,163 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace xubot_core.src.Globals
+{
+	/*
+	<article name="[article name]" lasteditor="[id]">
+		Article content.
+	</article>
+	*/
+
+	public class Wiki : ModuleBase
+	{
+		[Group("wiki")]
+		public class WikiCmd : ModuleBase
+		{
+			[Command]
+			public async Task Get(string article)
+			{
+				EmbedBuilder embedd = WikiTools.BuildEmbed(WikiTools.ReadArticle(article), WikiTools.ReadLastEdit(article), article);
+				await ReplyAsync("", false, embedd.Build());
+			}
+
+			[Command("edit")]
+			public async Task Edit(string article, string content)
+			{
+				WikiTools.AddEditArticle(Context, article, content);
+				await ReplyAsync("Article (hopefully) added.");
+			}
+		}
+	}
+
+	public class WikiTools
+	{
+		public static EmbedBuilder BuildEmbed(string article, string lasteditor, string name)
+		{
+			return new EmbedBuilder
+			{
+				Title = "Wiki",
+				Color = Discord.Color.Gold,
+				Description = @"Article: ""**" + name.ToLower() + @"**"". Was last edited by **" + lasteditor + "**",
+
+				Footer = new EmbedFooterBuilder
+				{
+					Text = "xubot :p"
+				},
+				Timestamp = DateTime.UtcNow,
+				Fields = new List<EmbedFieldBuilder>()
+						{
+							new EmbedFieldBuilder
+							{
+								Name = name.ToLower(),
+								Value = article,
+								IsInline = false
+							}
+						}
+			};
+		}
+
+		public static string ReadArticle(string article)
+		{
+			var xdoc = XDocument.Load("Wiki.xml");
+
+			var items = from i in xdoc.Descendants("article")
+						select new
+						{
+							name = (string)i.Attribute("name"),
+							lasteditor = (string)i.Attribute("lasteditor"),
+							i.Value
+						};
+
+			foreach (var item in items)
+			{
+				if (item.name.ToLower() == article.ToLower())
+				{
+					return item.Value;
+				}
+			}
+
+			return "No article exists! Create one!";
+		}
+		public static string ReadLastEdit(string article)
+		{
+			var xdoc = XDocument.Load("Wiki.xml");
+
+			var items = from i in xdoc.Descendants("article")
+						select new
+						{
+							name = (string)i.Attribute("name"),
+							lasteditor = (string)i.Attribute("lasteditor"),
+							i.Value
+						};
+
+			foreach (var item in items)
+			{
+				if (item.name.ToLower() == article.ToLower())
+				{
+					return item.lasteditor;
+				}
+			}
+
+			return "noone";
+		}
+
+		public static async void AddEditArticle(ICommandContext Context, string article, string content)
+		{
+			bool exist = false;
+			var xdoc = XDocument.Load("Wiki.xml");
+
+			var items = from i in xdoc.Descendants("article")
+						select new
+						{
+							name = i.Attribute("name"),
+							lasteditor = i.Attribute("lasteditor"),
+							content = i.Value
+						};
+
+			foreach (var item in items)
+			{
+				if (((string)item.name).ToLower() == article.ToLower())
+				{
+					exist = true;
+					try
+					{
+						xdoc.XPathSelectElement("//article[@name='" + article.ToLower() + "']").Value = content;
+						item.lasteditor.Value = Context.Message.Author.Username + "#" + Context.Message.Author.Discriminator;
+						xdoc.Save("Wiki.xml");
+					}
+					catch (Exception exp)
+					{
+						await Util.Error.BuildError(exp, Context);
+					}
+
+					await Task.CompletedTask;
+				}
+			}
+
+			if (!exist)
+			{
+				XElement element = new XElement("article");
+
+				XAttribute name_att = new XAttribute("name", article);
+				XAttribute editor_att = new XAttribute("lasteditor", Context.Message.Author);
+
+				element.Add(name_att);
+				element.Add(editor_att);
+				element.SetValue(content);
+
+				xdoc.Root.Add(element);
+				xdoc.Save("Wiki.xml");
+
+				await Task.CompletedTask;
+			}
+		}
+	}
+}
