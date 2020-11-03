543a9e11ead1b7f9f22c27e7e23f2f18d8839409
xubiod <xubiod.mail@gmail.com>
2020-11-03 18:05:49 -0500

    look at this

---
+++ DebugModule/Entrypoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using XubotSharedModule;
+
+namespace DebugModule
+{
+    public class Entrypoint : BotModule
+    {
+        public object Load()
+        {
+            return "Success on load";
+        }
+    }
+}
---
+++ XubotSharedModule/BotModule.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XubotSharedModule
+{
+    public interface BotModule
+    {
+        public object Load();
+    }
+}
