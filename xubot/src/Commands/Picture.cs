20a28bb4a3e027192af363dab549274bf95bf54b
xubiod <xubiod.mail@gmail.com>
2021-03-05 16:39:45 -0500

    Revert "fixed double space in help"
    This reverts commit c0d526ba166d5151eafa670ae838c213cfe3a074.

--- xubot/src/Commands/Picture.cs
+++ xubot/src/Commands/Picture.cs
@@ -19,7 +19,7 @@ using System.Collections.Generic;
 
 namespace xubot.src.Commands
 {
-    public class Picture
+    public class Picture : ModuleBase
     {
         [Group("pic"), Summary("Does shit with images.")]
         public class PictureCMD : ModuleBase
