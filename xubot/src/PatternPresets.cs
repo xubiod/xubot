0df88f7d9a0436b0172896635660faa5ad5e2077
xubiod <xubiod.mail@gmail.com>
2021-03-06 22:19:00 -0500

    i think this is less UGLY as FUCK

--- xubot/src/PatternPresets.cs
+++ xubot/src/PatternPresets.cs
@@ -13,194 +13,47 @@ namespace xubot.src
             switch (searchterm)
             {
                 case "checker":
-                    {
-                        if (patternID == 1) { return "10101"; }
-                        if (patternID == 2) { return "01010"; }
-                        if (patternID == 3) { return "10101"; }
-                        if (patternID == 4) { return "01010"; }
-                        if (patternID == 5) { return "10101"; }
-                        return "FAILURE";
-                    }
+                    return new string[] { "10101", "01010", "01010", "01010", "10101" }[(patternID - 1) % 5];
                 case "carrot-r":
-                    {
-                        if (patternID == 1) { return "01100"; }
-                        if (patternID == 2) { return "00110"; }
-                        if (patternID == 3) { return "00011"; }
-                        if (patternID == 4) { return "00110"; }
-                        if (patternID == 5) { return "01100"; }
-                        return "FAILURE";
-                    }
+                    return new string[] { "01100", "00110", "00011", "00110", "01100" }[(patternID - 1) % 5];
                 case "carrot-l":
-                    {
-                        if (patternID == 1) { return "00011"; }
-                        if (patternID == 2) { return "00110"; }
-                        if (patternID == 3) { return "01100"; }
-                        if (patternID == 4) { return "00110"; }
-                        if (patternID == 5) { return "00011"; }
-                        return "FAILURE";
-                    }
+                    return new string[] { "00011", "00110", "01100", "00110", "00011" }[(patternID - 1) % 5];
                 case "power":
-                    {
-                        if (patternID == 1) { return "00100"; }
-                        if (patternID == 2) { return "10101"; }
-                        if (patternID == 3) { return "10101"; }
-                        if (patternID == 4) { return "10001"; }
-                        if (patternID == 5) { return "01110"; }
-                        return "FAILURE";
-                    }
+                    return new string[] { "00100", "10101", "10101", "10001", "01110" }[(patternID - 1) % 5];
                 case "pico":
-                    {
-                        if (patternID == 1) { return "00100"; }
-                        if (patternID == 2) { return "01010"; }
-                        if (patternID == 3) { return "10001"; }
-                        if (patternID == 4) { return "01010"; }
-                        if (patternID == 5) { return "00100"; }
-                        return "FAILURE";
-                    }
+                    return new string[] { "00100", "01010", "10001", "01010", "00100" }[(patternID - 1) % 5];
                 case "cross-hair":
-                    {
-                        if (patternID == 1) { return "01010"; }
-                        if (patternID == 2) { return "10001"; }
-                        if (patternID == 3) { return "00100"; }
-                        if (patternID == 4) { return "10001"; }
-                        if (patternID == 5) { return "01010"; }
-                        return "FAILURE";
-                    }
+                    return new string[] { "01010", "10001", "00100", "10001", "01010" }[(patternID - 1) % 5];
                 case "ship":
-                    {
-                        if (patternID == 1) { return "00100"; }
-                        if (patternID == 2) { return "01010"; }
-                        if (patternID == 3) { return "11011"; }
-                        if (patternID == 4) { return "11111"; }
-                        if (patternID == 5) { return "10101"; }
-                        return "FAILURE";
-                    }
+                    return new string[] { "00100", "01010", "11011", "11111", "10101" }[(patternID - 1) % 5];
                 case "smile-c":
-                    {
-                        if (patternID == 1) { return "00000"; }
-                        if (patternID == 2) { return "01010"; }
-                        if (patternID == 3) { return "00000"; }
-                        if (patternID == 4) { return "10001"; }
-                        if (patternID == 5) { return "01110"; }
-                        return "FAILURE";
-                    }
+                    return new string[] { "00000", "01010", "00000", "10001", "01110" }[(patternID - 1) % 5];
                 case "smile-o":
-                    {
-                        if (patternID == 1) { return "00000"; }
-                        if (patternID == 2) { return "01010"; }
-                        if (patternID == 3) { return "00000"; }
-                        if (patternID == 4) { return "11111"; }
-                        if (patternID == 5) { return "01110"; }
-                        return "FAILURE";
-                    }
+                    return new string[] { "00000", "01010", "00000", "11111", "01110" }[(patternID - 1) % 5];
                 case "sad-c":
-                    {
-                        if (patternID == 1) { return "00000"; }
-                        if (patternID == 2) { return "01010"; }
-                        if (patternID == 3) { return "00000"; }
-                        if (patternID == 4) { return "01110"; }
-                        if (patternID == 5) { return "10001"; }
-                        return "FAILURE";
-                    }
+                    return new string[] { "00000", "01010", "00000", "01110", "10001" }[(patternID - 1) % 5];
                 case "sad-o":
-                    {
-                        if (patternID == 1) { return "00000"; }
-                        if (patternID == 2) { return "01010"; }
-                        if (patternID == 3) { return "00000"; }
-                        if (patternID == 4) { return "01110"; }
-                        if (patternID == 5) { return "11111"; }
-                        return "FAILURE";
-                    }
+                    return new string[] { "00000", "01010", "00000", "01110", "11111" }[(patternID - 1) % 5];
                 case "spade":
-                    {
-                        if (patternID == 1) { return "00100"; }
-                        if (patternID == 2) { return "01110"; }
-                        if (patternID == 3) { return "11111"; }
-                        if (patternID == 4) { return "11111"; }
-                        if (patternID == 5) { return "00100"; }
-                        return "FAILURE";
-                    }
+                    return new string[] { "00100", "01110", "11111", "11111", "00100" }[(patternID - 1) % 5];
                 case "club":
-                    {
-                        if (patternID == 1) { return "11011"; }
-                        if (patternID == 2) { return "11011"; }
-                        if (patternID == 3) { return "00100"; }
-                        if (patternID == 4) { return "01011"; }
-                        if (patternID == 5) { return "10011"; }
-                        return "FAILURE";
-                    }
+                    return new string[] { "11011", "11011", "00100", "01011", "10011" }[(patternID - 1) % 5];
                 case "heart":
-                    {
-                        if (patternID == 1) { return "01010"; }
-                        if (patternID == 2) { return "11111"; }
-                        if (patternID == 3) { return "11111"; }
-                        if (patternID == 4) { return "01110"; }
-                        if (patternID == 5) { return "00100"; }
-                        return "FAILURE";
-                    }
+                    return new string[] { "01010", "11111", "11111", "01110", "00100" }[(patternID - 1) % 5];
                 case "diamond":
-                    {
-                        if (patternID == 1) { return "00100"; }
-                        if (patternID == 2) { return "01110"; }
-                        if (patternID == 3) { return "11111"; }
-                        if (patternID == 4) { return "01110"; }
-                        if (patternID == 5) { return "00100"; }
-                        return "FAILURE";
-                    }
+                    return new string[] { "00100", "01110", "11111", "01110", "00100" }[(patternID - 1) % 5];
                 case "eighth-note":
-                    {
-                        if (patternID == 1) { return "00110"; }
-                        if (patternID == 2) { return "00100"; }
-                        if (patternID == 3) { return "00100"; }
-                        if (patternID == 4) { return "01100"; }
-                        if (patternID == 5) { return "01100"; }
-                        return "FAILURE";
-                    }
+                    return new string[] { "00110", "00100", "00100", "01100", "01100" }[(patternID - 1) % 5];
                 case "sixteenth-note":
-                    {
-                        if (patternID == 1) { return "00110"; }
-                        if (patternID == 2) { return "00110"; }
-                        if (patternID == 3) { return "00100"; }
-                        if (patternID == 4) { return "01100"; }
-                        if (patternID == 5) { return "01100"; }
-                        return "FAILURE";
-                    }
+                    return new string[] { "00110", "00110", "00100", "01100", "01100" }[(patternID - 1) % 5];
                 case "double-eighth-note":
-                    {
-                        if (patternID == 1) { return "01111"; }
-                        if (patternID == 2) { return "01001"; }
-                        if (patternID == 3) { return "01001"; }
-                        if (patternID == 4) { return "11011"; }
-                        if (patternID == 5) { return "11011"; }
-                        return "FAILURE";
-                    }
+                    return new string[] { "01111", "01001", "01001", "11011", "11011" }[(patternID - 1) % 5];
                 case "double-sixteenth-note":
-                    {
-                        if (patternID == 1) { return "01111"; }
-                        if (patternID == 2) { return "01111"; }
-                        if (patternID == 3) { return "01001"; }
-                        if (patternID == 4) { return "11011"; }
-                        if (patternID == 5) { return "11011"; }
-                        return "FAILURE";
-                    }
+                    return new string[] { "01111", "01111", "01001", "11011", "11011" }[(patternID - 1) % 5];
                 case "hammer":
-                    {
-                        if (patternID == 1) { return "01101"; }
-                        if (patternID == 2) { return "11111"; }
-                        if (patternID == 3) { return "10101"; }
-                        if (patternID == 4) { return "00100"; }
-                        if (patternID == 5) { return "00100"; }
-                        return "FAILURE";
-                    }
+                    return new string[] { "01101", "11111", "10101", "00100", "00100" }[(patternID - 1) % 5];
                 case "paper":
-                    {
-                        if (patternID == 1) { return "11111"; }
-                        if (patternID == 2) { return "10001"; }
-                        if (patternID == 3) { return "11111"; }
-                        if (patternID == 4) { return "10001"; }
-                        if (patternID == 5) { return "11111"; }
-                        return "FAILURE";
-                    }
+                    return new string[] { "11111", "10001", "11111", "10001", "11111" }[(patternID - 1) % 5];
                 default: return "FAILURE";
             }
         }
