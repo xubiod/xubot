fc8ac4351bc3cb1ef680dd5913cd8f922ae3621f
xubiod <xubiod.mail@gmail.com>
2021-03-10 21:17:03 -0500

    a new attribute future me is going to have fun implementing!

---
+++ xubot/src/Attributes/ExampleAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xubot.src.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = false)]
+    public class ExampleAttribute : Attribute
+    {
+        public string ExampleCmdUsage { get; }
+
+        public ExampleAttribute(string examplecmd)
+        {
+            this.ExampleCmdUsage = examplecmd;
+        }
+
+        public string GetExampleUsage()
+        {
+            return ExampleCmdUsage;
+        }
+    }
+}
