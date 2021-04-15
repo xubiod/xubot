ed4548fce5e81f425c0aa0cbfa42258ea717cc40
xubiod <xubiod.mail@gmail.com>
2021-04-15 09:51:27 -0400

    more types in SimplifyTypes for the lols

--- xubot/src/Util.cs
+++ xubot/src/Util.cs
@@ -249,15 +249,23 @@ namespace xubot.src
             {
                 switch (input)
                 {
-                    case "System.String": return "string";
                     case "System.Boolean": return "bool";
+                    case "System.Byte": return "byte";
+                    case "System.SByte": return "sbyte";
+                    case "System.Char": return "char";
                     case "System.Decimal": return "decimal";
-                    case "System.Single": return "float";
                     case "System.Double": return "double";
+                    case "System.Single": return "float";
                     case "System.Int32": return "int";
                     case "System.UInt32": return "uint";
+                    case "System.IntPtr": return "nint";
+                    case "System.UIntPtr": return "nuint";
                     case "System.Int64": return "long";
                     case "System.UInt64": return "ulong";
+                    case "System.Int16": return "short";
+                    case "System.UInt16": return "ushort";
+                    case "System.String": return "string";
+                    case "System.Object": return "object";
 
                     default: return input;
                 }
